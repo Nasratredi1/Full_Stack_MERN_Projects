99572b1dbc82f74d1fd095478282ac9262242dcf
Nasratredi1 <73361214+Nasratredi1@users.noreply.github.com>
2022-02-20 01:18:14 +0500

    Front and back end Deendency

---
+++ Server/index.js
@@ -0,0 +1,4 @@
+import express from 'express';   // before that we can used const express = requires ('express');
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
